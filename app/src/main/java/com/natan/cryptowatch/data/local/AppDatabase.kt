a90bc60cd2de26174dd3afe5767be88a9fc7e909
miolanatan5-commits <miolanatan5@gmail.com>
2026-09-04 13:55:05 -0300

    Add app/src/main/java/com/natan/cryptowatch/data/local/AppDatabase.kt

---
+++ app/src/main/java/com/natan/cryptowatch/data/local/AppDatabase.kt
@@ -0,0 +1,7 @@
+package com.natan.cryptowatch.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [FavoriteCoin::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() { abstract fun favoriteDao(): FavoriteDao }
